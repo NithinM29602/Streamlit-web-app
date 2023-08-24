cc2fbdeb8bbe1d83c5bffaaeb1921cc33e26f274
Nithin M <92316309+NithinM29602@users.noreply.github.com>
2023-08-25 01:28:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
 st.header("Welcome to my World")
+
+import cv2
+import streamlit as st
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+st.title("Webcam Video Stream")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Perform your processing on 'frame' here
+
+    # Display the frame in Streamlit
+    st.image(frame, channels="BGR", use_column_width=True)
+
+cap.release()
